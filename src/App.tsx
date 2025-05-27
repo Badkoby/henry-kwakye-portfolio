884580228264b448a31e966ba7a88e8fd1ae996e
Henry Kwakye <iamkobinahenry@gmail.com>
2025-05-27 08:56:45 +0100

    Fix: Add index route to show homepage by default

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
               }}
             >
               <Routes>
+                <Route index element={<Home />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/projects" element={<Projects />} />
